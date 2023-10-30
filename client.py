6d9b22cbe17cca676c23108936e0a7540a4cc2a3
anderson-mj <anderson_meireles@id.uff.br>
2023-10-30 16:49:23 -0300

    Refactor client.py to use a ChatClient class

--- client.py
+++ client.py
@@ -1,104 +1,70 @@
 import socket
 import tkinter as tk
 
-# Cria um socket TCP/IP
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class ChatClient:
+    def __init__(self, server_address=('localhost', 3001)):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect(server_address)
+        self.ip, self.port = self.client_socket.getsockname()
+        self.root = tk.Tk()
+        self.root.title('Chat Client')
+        self.user_list = tk.Listbox(self.root)
+        self.user_list.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.user_list.bind('<Double-1>', self.callback)
 
-# Define o endereço e porta do servidor
-server_address = ('localhost', 3000)
+        self.list_button = tk.Button(self.root, text='Consultar usuários', command=self.show_user_list)
+        self.list_button.pack(side=tk.LEFT)
 
-# Conecta o socket ao servidor
-client_socket.connect(server_address)
+        self.quit_button = tk.Button(self.root, text='Encerrar conexão', command=lambda: self.send_message('quit'))
+        self.quit_button.pack(side=tk.LEFT)
 
-# Obtém o IP e porta do cliente
-ip, port = client_socket.getsockname()
+    def register(self, name, client_call_port):
+        message = ','.join([name, self.ip, str(self.port), str(client_call_port)])
+        self.client_socket.send(message.encode())
+        response = self.client_socket.recv(1024).decode().split(',')
+        print(response[1])
+        if response[0] == 'error':
+            self.client_socket.close()
+            exit()
 
-# Envia uma mensagem de registro para o servidor
-name = input('Qual o seu nome? ')
-client_call_port = int(input('Qual porta que você deseja usar para receber chamadas? '))
-message = ','.join([name, ip, str(port), str(client_call_port)])
-client_socket.send(message.encode())
+    def callback(self, event):
+        selection = event.widget.curselection()
+        if selection:
+            index = selection[0]
+            data = event.widget.get(index)
+            self.show_user_details(data)
 
-# Recebe uma resposta do servidor
-response = client_socket.recv(1024).decode().split(',')
-print(response[1])
-if response[0] == 'error':
-    client_socket.close()
-    exit()
+    def show_user_list(self):
+        self.send_message('list')
+        response = self.client_socket.recv(1024).decode()
+        self.user_list.delete(0, tk.END)
+        for user in response.split(','):
+            self.user_list.insert(tk.END, user)
+        self.root.update()
 
-# Cria a interface gráfica
-root = tk.Tk()
-root.title('Chat Client')
+    def show_user_details(self, user):
+        self.send_message(f'details,{user}')
+        response = self.client_socket.recv(1024).decode()
+        ip, port = response.split(',')
+        detail_window = tk.Toplevel(self.root)
+        detail_window.title(f"Detalhes de {user}")
+        detail_label = tk.Label(detail_window, text=f"IP: {ip}\nPorta: {port}")
+        detail_label.pack(side=tk.TOP)
 
-# Cria o botão de consulta de usuários
-list_button = tk.Button(root, text='Consultar usuários', command=lambda: show_user_list())
-list_button.pack(side=tk.LEFT)
+    def send_message(self, message):
+        self.client_socket.send(message.encode())
+        if message == 'quit':
+            response = self.client_socket.recv(1024).decode()
+            print(response)
+            self.client_socket.close()
+            self.root.quit()
 
-# Cria o botão de encerramento da conexão
-quit_button = tk.Button(root, text='Encerrar conexão', command=lambda: send_message('quit'))
-quit_button.pack(side=tk.LEFT)
+    def run(self):
+        self.root.mainloop()
 
-# Cria a tabela de usuários
-user_list = tk.Listbox(root)
-user_list.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-
-def callback(event):
-    selection = event.widget.curselection()
-    if selection:
-        index = selection[0]
-        data = event.widget.get(index)
-        show_user_details(data)
-    else:
-        print(0)
-
-user_list.bind('<Double-1>', callback)
-
-# Exibe a lista de usuários
-def show_user_list():
-    global user_list
-
-    # Envia uma mensagem de consulta de usuários para o servidor
-    send_message('list')
-
-    # Recebe a lista de usuários do servidor
-    response = client_socket.recv(1024).decode()
-    user_list.delete(0, tk.END)
-
-    # Adiciona os usuários à lista
-    for user in response.split(','):
-        user_list.insert(tk.END, user)
-
-    # Atualiza a interface gráfica
-    root.update()
-
-# Exibe os detalhes de um usuário
-def show_user_details(user):
-    global detail_window
-    global close_button
-
-    # Envia uma mensagem de consulta de detalhes para o servidor
-    send_message(f'details,{user}')
-
-    # Recebe os detalhes do usuário do servidor
-    response = client_socket.recv(1024).decode()
-    ip, port = response.split(',')
-
-    # Cria a janela de detalhes do usuário
-    detail_window = tk.Toplevel(root)
-    detail_window.title(f"Detalhes de {user}")
-    detail_label = tk.Label(detail_window, text=f"IP: {ip}\nPorta: {port}")
-    detail_label.pack(side=tk.TOP)
-
-# Envia mensagens para o servidor
-def send_message(message):
-    global client_socket
-
-    client_socket.send(message.encode())
-
-    if message == 'quit':
-        response = client_socket.recv(1024).decode()
-        print(response)
-        client_socket.close()
-        root.quit()
-
-root.mainloop()
+if __name__ == "__main__":
+    name = input('Qual o seu nome? ')
+    client_call_port = int(input('Qual porta que você deseja usar para receber chamadas? '))
+    client = ChatClient()
+    client.register(name, client_call_port)
+    client.run()
